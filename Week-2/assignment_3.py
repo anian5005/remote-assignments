b61ecc357ce740bd622e5ed4afa3510f0921409b
A-Nian <97599669+anian5005@users.noreply.github.com>
2022-04-01 15:20:00 +0800

    FIX: 分母 Hardcode

--- Week-2/assignment_3.py
+++ Week-2/assignment_3.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[2]:
 
 
 def avg(data):
     key = list(data.keys())[0] #除了list有其他方法嗎? #只能適用單一結構嗎?
     products = data[key]
-    print(products)
+    all_num = len(products)
+    #print(products)
 
     all_price = 0
     for product in products:
         price = product['price']
         all_price = all_price + price
 
-    avg = round(all_price/3, 3)
+    avg = round(all_price/all_num, 3)
     print(avg)
 
 
-# In[2]:
+# In[3]:
 
 
 avg({
@@ -39,23 +40,10 @@ avg({
  })
 
 
-# In[4]:
+# In[ ]:
 
 
-#product = ["Product 1","Product 1","Product 2"]
 
-price = [100,700,300]
-    
-product={product,price}
-
-print(product)
-product = ["Product 1","Product 1","Product 2"]
-price = [100,700,300]
-    
-    
-product={product,price}
-
-print(product)
 
 
 # In[ ]:
